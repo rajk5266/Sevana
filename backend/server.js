c0bb09328a9a597762744561c70f0e6a9f2676a2
Raj Kushwaha <rajkushwaha@MacBookAir.lan>
2025-01-13 23:06:14 +0530

    port changes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 // Middleware
 app.use(express.json());
